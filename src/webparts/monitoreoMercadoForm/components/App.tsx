57fea5c03f2878f800ec8b6fbfc424b276bd7482
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-07-17 10:02:23 -0400

    Added </hr> on App

--- src/webparts/monitoreoMercadoForm/components/App.tsx
+++ src/webparts/monitoreoMercadoForm/components/App.tsx
@@ -51,6 +51,7 @@ const App: React.FC<AppProps> = (props) => {
         isEmpty(props.listaPeriodosCultivo) ? (
           <>
             <Title1>Error en la Aplicación</Title1>
+            <hr/>
             <Title2>Comuníquese con el Departamento de T.I.</Title2>
           </>
       ) : (
