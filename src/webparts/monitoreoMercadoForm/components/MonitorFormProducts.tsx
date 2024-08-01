22b88f39dcd39055c4a00f5da082b8734de632f2
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-08-01 16:42:29 -0400

    MonitorFormProducts component inputs and comboboxes fully controlled

--- src/webparts/monitoreoMercadoForm/components/MonitorFormProducts.tsx
+++ src/webparts/monitoreoMercadoForm/components/MonitorFormProducts.tsx
@@ -1,33 +1,41 @@
-import * as React from "react";
+import * as React from 'react';
 
-import { Input, Combobox, Option, Title2, Button, Text, Body2, makeStyles, Field } from "@fluentui/react-components";
+import {
+	Input,
+	Combobox,
+	Option,
+	Title2,
+	Button,
+	Text,
+	Body2,
+	makeStyles,
+	Field,
+	SelectionEvents,
+	OptionOnSelectData,
+	InputOnChangeData,
+} from '@fluentui/react-components';
 import {
 	ArrowLeftFilled,
 	ArrowRightFilled,
 	SaveRegular,
 } from '@fluentui/react-icons';
 
-import { IFamiliaProducto } from "./interfaces/IFamiliaProducto";
-import { IProveedor } from "./interfaces/IProveedor";
-import { IPeriodoCultivo } from "./interfaces/IPeriodoCultivo";
+import { IFamiliaProducto } from './interfaces/IFamiliaProducto';
+import { IProveedor } from './interfaces/IProveedor';
+import { IPeriodoCultivo } from './interfaces/IPeriodoCultivo';
 
-
-export interface IMonitorFormProductsProps{
-  listaFamiliasProducto:IFamiliaProducto[],
-  listaProveedores:IProveedor[],
-  periodoCultivo:IPeriodoCultivo|undefined,
+export interface IMonitorFormProductsProps {
+	listaFamiliasProducto: IFamiliaProducto[];
+	listaProveedores: IProveedor[];
+	periodoCultivo: IPeriodoCultivo | undefined;
 }
 
-interface IProductValueState{
-  familiaProducto:IFamiliaProducto|undefined,
-  volumenComprado:string|undefined,
-  precioPorMedida:number|undefined,
-  condicionPago:string|undefined,
-  proveedorPrincipal:IProveedor|undefined,
-}
-
-interface IMonitorFormProductsState{
-  valoresFormProducts:(IProductValueState|undefined)[],
+interface IProductValueState {
+	familiaProducto: IFamiliaProducto | undefined;
+	volumenComprado: string | undefined;
+	precioPorMedida: number | undefined;
+	condicionPago: string | undefined;
+	proveedorPrincipal: IProveedor | undefined;
 }
 
 const useStyles = makeStyles({
@@ -51,155 +59,224 @@ const useStyles = makeStyles({
 	},
 });
 
-const MonitorFormProducts:React.FC<IMonitorFormProductsProps> = (props) =>{
-  const {listaFamiliasProducto, listaProveedores, periodoCultivo} = props
-  const volumen:number[] = [...Array(11).keys()].map((value:number) => value * 10)
-// [
-//   estadoUnProducto:IProductValueState,
-//   estadoUnProducto:IProductValueState
-//   estadoUnProducto:IProductValueState
-//   estadoUnProducto:IProductValueState
-//   estadoUnProducto:IProductValueState
-// ]
+const MonitorFormProducts: React.FC<IMonitorFormProductsProps> = (props) => {
+	const { listaFamiliasProducto, listaProveedores, periodoCultivo } = props;
+	const volumen: number[] = [...Array(11).keys()].map(
+		(value: number) => value * 10,
+	);
 
+	//Component Style
+	const styles = useStyles();
 
-  //Component Style
-  const styles = useStyles()
+	const listaProductosFiltro: IFamiliaProducto[] =
+		listaFamiliasProducto.filter(
+			(item: IFamiliaProducto) =>
+				item.PeriodoCultivo?.Id === periodoCultivo?.Id,
+		);
+	const largoLista = listaProductosFiltro.length;
 
-  const [productValues, setProductValues] = React.useState<IMonitorFormProductsState>({} as IMonitorFormProductsState)
-  const [index, setIndex] = React.useState<number>(0);
-  const listaProductosFiltro:IFamiliaProducto[] = listaFamiliasProducto.filter((item:IFamiliaProducto) => item.PeriodoCultivo?.Id === periodoCultivo?.Id)
+	const [productValues, setProductValues] = React.useState<
+		IProductValueState[]
+	>(Array.from({ length: largoLista }, () => ({} as IProductValueState)));
+	const [index, setIndex] = React.useState<number>(0);
 
-  const largoLista = listaProductosFiltro.length
-  
-  const handleBtnRetrocesoClick = React.useCallback(() =>{
-    const indexNuevo = index-1 > -1 ? index-1 : index
-    setIndex(indexNuevo)
-    
-    // let objAux:IProductValueState = {} as IProductValueState
+	const handleBtnRetrocesoClick = React.useCallback(() => {
+		const indexNuevo = index - 1 > -1 ? index - 1 : index;
+		setIndex(indexNuevo);
+	}, [setIndex]);
 
-    // objAux = productValues.valoresFormProducts[indexNuevo]
+	const handleBtnAvanzarClick = React.useCallback(() => {
+		const indexNuevo = index + 1 < largoLista ? index + 1 : index;
+		setIndex(indexNuevo);
+	}, [setIndex]);
 
-    // setProductValues({
-    //   ...productValues, 
-    //   valoresFormProducts: [...productValues.valoresFormProducts]
-    // })
+	const handleSelectedChanges = (
+		campo: keyof IProductValueState,
+		valor: string | undefined,
+	): void => {
+		let objAux: IProductValueState[];
+		console.log(productValues);
+		switch (campo) {
+			case 'condicionPago':
+				objAux = [...productValues];
 
-  },
-    [setIndex]
-  )
+				objAux.map((productValue: IProductValueState, i: number) => {
+					if (i === index) {
+						objAux[i].condicionPago = valor;
+					}
+				});
 
-  const handleBtnAvanzarClick = React.useCallback(()=>{
-    const indexNuevo = index+1 < largoLista ? index+1 : index
-    setIndex(indexNuevo)
-    
-      let objAux:IProductValueState|undefined = {} as IProductValueState
+				setProductValues(objAux);
+				break;
 
-      objAux = productValues.valoresFormProducts[indexNuevo]
+				setProductValues(objAux);
+				break;
+			case 'precioPorMedida':
+				objAux = [...productValues];
 
-      setProductValues({
-        ...productValues, 
-        valoresFormProducts: [objAux]
-      })
-  },
-    [setIndex]
-  )
+				objAux.map((productValue: IProductValueState, i: number) => {
+					if (i === index) {
+						objAux[i].precioPorMedida = valor ? +valor : undefined;
+					}
+				});
 
-  // const btnPasarClick = (): void => {
-  //   if (index < largoLista - 1) {
-  //     const indexNuevo = index + 1;
-  //     const familiaNueva = actualizarFamilia(indexNuevo);
-  //     setFamiliaActiva(familiaNueva);
-  //     setValoresForm(prevValores => {
-  //       const nuevosValores = [...prevValores];
-  //       nuevosValores[index] = { ...nuevosValores[index], idFamiliaProducto: familiaActiva.Id };
-  //       return nuevosValores;
-  //     });
-  //     setIndex(indexNuevo);
-  //   } else {
-  //     const listaValoresProductos = actualizarValores(valoresForm);
+				setProductValues(objAux);
+				break;
+			case 'proveedorPrincipal':
+				objAux = [...productValues];
 
-  //     const dataGuardar:DatosValores[] = listaValoresProductos.map(
-  //       (valoresProducto: FamiliasValores)  => ({
-  //         idCliente: cliente,
-  //         idUnidad: unidad,
-  //         idFamilia: valoresProducto.idFamiliaProducto,
-  //         volumenComprado: valoresProducto.volumenComprado,
-  //         precio: valoresProducto.precio,
-  //         condicionPago: valoresProducto.condicionPago,
-  //         proveedorPrincipal: valoresProducto.proveedorPrincipal,
-  //   }));
-  //     dataGuardar.map((data:DatosValores)=>{
-  //       onSave(data);
-  //       reset();
-  //     })
-  //   }
-  // };
+				objAux.map((productValue: IProductValueState, i: number) => {
+					if (i === index) {
+						objAux[i].proveedorPrincipal = listaProveedores.find(
+							(proveedor: IProveedor) =>
+								proveedor.Id === (valor ? +valor : undefined),
+						);
+					}
+				});
 
+				setProductValues(objAux);
+				break;
+			case 'volumenComprado':
+				objAux = [...productValues];
 
-  console.log(productValues.valoresFormProducts[index] || '')
+				objAux.map((productValue: IProductValueState, i: number) => {
+					if (i === index) {
+						objAux[i].volumenComprado = valor;
+					}
+				});
 
-  return(
-    <section>
-      <Title2>
-        {listaProductosFiltro[index].Nombre}
-      </Title2>
-        <form>
-          <Field
-            label={'Volumen Ya Comprado'}
-            required
-          >
-            <Combobox
-              placeholder={`Seleccione volumen ya comprado`}
-              value={productValues?.valoresFormProducts[index]?.volumenComprado || ''}
-            >
-              {
-                volumen.map((item:number) => (
-                  <Option value={`${item}%`} key={item}>
-                    {`${item}%`}
-                  </Option>
-                ))
-              }
-            </Combobox>
-          </Field>
-          <Field
-            label={`Precio por ${listaProductosFiltro[index].UnidadMedida}`}
-            required
-          >
-              <Input 
-                placeholder={`Seleccionar precio por ${listaProductosFiltro[index].UnidadMedida}`}
-                value={productValues?.valoresFormProducts[index]?.precioPorMedida?.toString() || ''}
-              />
-          </Field>
-          <Field
-            label={`Condición de Pago`}
-            required
-          >
-            <Combobox
-            placeholder={'Seleccione condicion de pago'}
-            value={productValues?.valoresFormProducts[index]?.condicionPago || ''}
-            >
-              <Option>Crédito</Option>
-              <Option>Contado</Option>
-            </Combobox>
-          </Field>
-          <Field
-            label={`Proveedor Principal`}
-            required
-          >
-            <Combobox
-              placeholder="Seleccione Proveedor Principal"
-              value={productValues?.valoresFormProducts[index]?.proveedorPrincipal?.Nombre || ''}
-            >
-              {listaProveedores.map((item:IProveedor) => (
-                <Option key={item.Id} text='Opcion' value={item.Id?.toString()}>
-                  {item.Nombre}
-                </Option>
-              ))}
-            </Combobox>
-          </Field>
-        </form>
-      <div className={`${styles.BotonesContainer}`}>
+				setProductValues(objAux);
+				break;
+			default:
+				console.log('Campo no switcheable');
+				break;
+		}
+	};
+
+	const handleCbxChanges = React.useCallback(
+		(e: SelectionEvents, data: OptionOnSelectData) => {
+			const event: HTMLElement = e.target as HTMLElement;
+
+			let elementName;
+
+			elementName = document
+				.querySelector(
+					`[aria-controls=${event.parentElement?.getAttribute(
+						'id',
+					)}]`,
+				)
+				?.getAttribute('name');
+
+			const name =
+				elementName === null || elementName === undefined
+					? ''
+					: elementName;
+			const value = data.optionValue;
+
+			console.log(`Cambio en Dpdown ${name}, valor: ${value}`);
+
+			handleSelectedChanges(name as keyof IProductValueState, value);
+		},
+		[handleSelectedChanges],
+	);
+
+	const handleInputChanges = React.useCallback(
+		(e: React.ChangeEvent<HTMLInputElement>, data: InputOnChangeData) => {
+			const event: HTMLElement = e.target as HTMLElement;
+
+			const elementName = event.getAttribute('name');
+
+			const name =
+				elementName === null || elementName === undefined
+					? ''
+					: elementName;
+			const value = data.value;
+
+			console.log(`Cambio en Input ${name}, valor: ${value}`);
+
+			handleSelectedChanges(name as keyof IProductValueState, value);
+		},
+		[handleSelectedChanges],
+	);
+
+	return (
+		<section>
+			<Title2>{listaProductosFiltro[index].Nombre}</Title2>
+			<form>
+				<Field
+					label={'Volumen Ya Comprado'}
+					required
+				>
+					<Combobox
+						name='volumenComprado'
+						placeholder={`Seleccione volumen ya comprado`}
+						value={productValues[index]?.volumenComprado || ''}
+						onOptionSelect={handleCbxChanges}
+					>
+						{volumen.map((item: number) => (
+							<Option
+								value={`${item}%`}
+								key={item}
+							>
+								{`${item}%`}
+							</Option>
+						))}
+					</Combobox>
+				</Field>
+				<Field
+					label={`Precio por ${listaProductosFiltro[index].UnidadMedida}`}
+					required
+				>
+					<Input
+						name='precioPorMedida'
+						placeholder={`Seleccionar precio por ${listaProductosFiltro[index].UnidadMedida}`}
+						value={
+							productValues[index]?.precioPorMedida?.toString() ||
+							''
+						}
+						onChange={handleInputChanges}
+					/>
+				</Field>
+				<Field
+					label={`Condición de Pago`}
+					required
+				>
+					<Combobox
+						name='condicionPago'
+						placeholder={'Seleccione condicion de pago'}
+						value={productValues[index]?.condicionPago || ''}
+						onOptionSelect={handleCbxChanges}
+					>
+						<Option>Crédito</Option>
+						<Option>Contado</Option>
+					</Combobox>
+				</Field>
+				<Field
+					label={`Proveedor Principal`}
+					required
+				>
+					<Combobox
+						name='proveedorPrincipal'
+						placeholder='Seleccione Proveedor Principal'
+						value={
+							productValues[index]?.proveedorPrincipal?.Nombre ||
+							''
+						}
+						onOptionSelect={handleCbxChanges}
+					>
+						{listaProveedores.map((item: IProveedor) => (
+							<Option
+								key={item.Id}
+								text='Opcion'
+								value={item.Id?.toString()}
+							>
+								{item.Nombre}
+							</Option>
+						))}
+					</Combobox>
+				</Field>
+			</form>
+			<div className={`${styles.BotonesContainer}`}>
 				{index > 0 && (
 					<Button
 						className={styles.BotonRetroceso + '' + styles.Boton}
@@ -209,7 +286,12 @@ const MonitorFormProducts:React.FC<IMonitorFormProductsProps> = (props) =>{
 						icon={<ArrowLeftFilled />}
 						iconPosition='before'
 					>
-						<Text size={400} weight={'semibold'}>Página Anterior</Text>
+						<Text
+							size={400}
+							weight={'semibold'}
+						>
+							Página Anterior
+						</Text>
 					</Button>
 				)}
 				<Body2>
@@ -231,13 +313,18 @@ const MonitorFormProducts:React.FC<IMonitorFormProductsProps> = (props) =>{
 					}
 					iconPosition='after'
 				>
-					<Text size={400} weight={'semibold'}>
-						{index + 1 === largoLista ? 'Guardar' : 'Siguiente Página'}
+					<Text
+						size={400}
+						weight={'semibold'}
+					>
+						{index + 1 === largoLista
+							? 'Guardar'
+							: 'Siguiente Página'}
 					</Text>
 				</Button>
 			</div>
-    </section>
-  )
-}
+		</section>
+	);
+};
 
-export default MonitorFormProducts
+export default MonitorFormProducts;
