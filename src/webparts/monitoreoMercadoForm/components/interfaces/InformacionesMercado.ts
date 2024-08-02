c1476be4db14f8dfa91d9988804e836a57fe4d2c
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-08-02 17:04:21 -0400

    Change DatosValores interface name

---
+++ src/webparts/monitoreoMercadoForm/components/interfaces/InformacionesMercado.ts
@@ -0,0 +1,10 @@
+export interface InformacionMercado{
+    idCliente: number,
+    idUnidad: number,
+    idFamilia: number,
+    volumenComprado: string,
+    precioPorMedida: number,
+    condicionPago: string,
+    idProveedorPrincipal: number,
+    idPeriodoCultivo:number
+}
