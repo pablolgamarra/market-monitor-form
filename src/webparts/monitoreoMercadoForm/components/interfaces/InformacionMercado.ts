7ac35680c1e0c35a4d8c943bc0954dde2963087a
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-08-07 08:46:39 -0400

    Removed undefined option in fields of InformacionMercado

--- src/webparts/monitoreoMercadoForm/components/interfaces/InformacionMercado.ts
+++ src/webparts/monitoreoMercadoForm/components/interfaces/InformacionMercado.ts
@@ -1,10 +1,10 @@
 export interface InformacionMercado{
     idCliente: number,
     idUnidad: number,
+    idPeriodoCultivo:number
     idFamilia: number,
     volumenComprado: string,
     precioPorMedida: number,
     condicionPago: string,
     idProveedorPrincipal: number,
-    idPeriodoCultivo:number
 }
