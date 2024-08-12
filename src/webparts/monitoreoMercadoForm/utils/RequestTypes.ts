d46a6023160b49773a1f2ce0740d91d70c103446
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-08-12 17:25:16 -0400

    Request and response types updated

--- src/webparts/monitoreoMercadoForm/utils/RequestTypes.ts
+++ src/webparts/monitoreoMercadoForm/utils/RequestTypes.ts
@@ -1,23 +1,17 @@
+import { CondicionesPago } from "../enums/CondicionesPago";
+
 export interface InformacionMercadoRequest{
   value?: InformacionMercadoValue;
 }
 export interface InformacionMercadoValue {
-  Id?:                                number;
-  OData__ColorTag?:                   undefined;
-  ComplianceAssetId?:                 undefined;
-  VolumenYaComprado:                 string;
-  Condici_x00f3_nPago:               string;
-  Precio:                            number;
-  ClienteId:                         number;
-  Familia_x0020_de_x0020_ProductoId: number;
-  Proveedor_x0020_PrincipalId:       number;
-  Periodo_x0020_de_x0020_CultivoId:  number;
-  ID?:                                number;
-  Modified?:                          Date;
-  Created?:                           Date;
-  AuthorId?:                          number;
-  EditorId?:                          number;
-  OData__UIVersionString?:            string;
-  Attachments?:                       boolean;
-  GUID?:                              string;
+    Id:                                number;
+    ClienteId:                         number;
+    Familia_x0020_de_x0020_ProductoId: number;
+    VolumenYaComprado:                 string;
+    Precio:                            number;
+    Condici_x00f3_nPago:               CondicionesPago;
+    Proveedor_x0020_PrincipalId:       number;
+    Periodo_x0020_de_x0020_CultivoId:  number;
+    CNGId:                             undefined;
+    ID:                                number;
 }
--- src/webparts/monitoreoMercadoForm/utils/ResponseTypes.ts
+++ src/webparts/monitoreoMercadoForm/utils/ResponseTypes.ts
@@ -1,77 +1,49 @@
+import { Activo } from "../enums/Activo";
+
 export interface UnidadesResponse {
-	'@odata.context': string;
-	value: UnidadesResponseValue[];
+    value: UnidadesResponseValue[];
 }
 
 export interface UnidadesResponseValue {
-	'@odata.type': string;
-	'@odata.id': string;
-	'@odata.etag': string;
-	'@odata.editLink': string;
-	Id: number;
-	Title: string;
-	ID: number;
+    Id:    number;
+    Title: string;
+    ID:    number;
 }
 
 export interface ClientesResponse {
-	'@odata.context': string;
-	value: ClientesResponseValue[];
+    value: ClientesResponseValue[];
 }
+
 export interface ClientesResponseValue {
-	'@odata.type': string;
-	'@odata.id': string;
-	'@odata.etag': string;
-	'@odata.editLink': string;
-	Id: number;
-	Title: string;
-	RUC_x002f_CI: number;
-	UnidadId: number;
-	CoordinadorId: number;
-	ID: number;
+    Id:                           number;
+    Title:                        string;
+    Codigo_x0020_SAP:             number;
+    UnidadId:                     number;
+    Codigo_x0020_SAP_x0020_CNGId: number;
+    ID:                           number;
 }
 
 export interface FamiliaProductosResponse {
-	'@odata.context': string;
-	value: FamiliaProductosResponseValue[];
+    value: FamiliaProductosResponseValue[];
 }
 
 export interface FamiliaProductosResponseValue {
-	'@odata.type': string;
-	'@odata.id': string;
-	'@odata.etag': string;
-	'@odata.editLink': string;
-	Id: number;
-	Title: string;
-	UnidaddeMedida: string;
-	PeriododeCultivoId: number|undefined;
-	Activo: boolean;
-	ID: number;
-}
-
-export interface ProveedoresResponse {
-	'@odata.context': string;
-	value: ProveedoresResponseValue[];
-}
-
-export interface ProveedoresResponseValue {
-	Id: number;
-	Title: string;
-	ID: number;
+    Id:                 number;
+    Title:              string;
+    UnidaddeMedida:     string;
+    Activo:             Activo;
+    PeriododeCultivoId: number;
+    ID:                 number;
 }
 
 export interface PeriodosCultivoResponse {
-	'odata.metadata': string;
-	value: PeriodosCultivoResponseValue[];
+    value: PeriodosCultivoResponseValue[];
 }
 
 export interface PeriodosCultivoResponseValue {
-	'odata.type': string;
-	'odata.id': string;
-	'odata.etag': string;
-	'odata.editLink': string;
-	Id: number;
-	Title: string;
-	ID: number;
+    Id:    number;
+    Title: string;
+    ID:    number;
 }
 
 export interface CNGResponse {
@@ -79,22 +51,23 @@ export interface CNGResponse {
 }
 
 export interface CNGResponseValue {
-    FileSystemObjectType:     number;
-    Id:                       number;
-    ServerRedirectedEmbedUri: undefined;
-    ServerRedirectedEmbedUrl: string;
-    ContentTypeId:            string;
-    Title:                    string;
-    OData__ColorTag:          undefined;
-    ComplianceAssetId:        undefined;
-    Correo:                   string;
-    ID:                       number;
-    Modified:                 Date;
-    Created:                  Date;
-    AuthorId:                 number;
-    EditorId:                 number;
-    OData__UIVersionString:   string;
-    Attachments:              boolean;
-    GUID:                     string;
+    Id:        number;
+    Title:     string;
+    Correo:    string;
+    NombreCNG: string;
+    ID:        number;
 }
+
+export interface ProveedoresResponse {
+    value: ProveedoresResponseValue[];
+}
+
+export interface ProveedoresResponseValue {
+    Id:                               number;
+    Title:                            string;
+    Familia_x0020_de_x0020_ProductoId: number;
+    ID:                               number;
+}
+
+
 
