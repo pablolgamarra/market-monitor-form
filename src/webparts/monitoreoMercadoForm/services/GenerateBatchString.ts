7c5f80929d955b5d0e727cf0e39957ac66ffae99
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-08-29 08:49:40 -0400

    Add .join() function on generateBatchString

--- src/webparts/monitoreoMercadoForm/services/GenerateBatchString.ts
+++ src/webparts/monitoreoMercadoForm/services/GenerateBatchString.ts
@@ -15,28 +15,28 @@ export default function generateBatchString(
     ${data.map(
 		(item: InformacionMercado) =>
 			`--changeset_${changeID}
-    Content-Type: application/http
-    Content-Transfer-Encoding: binary
+			Content-Type: application/http
+			Content-Transfer-Encoding: binary
 
-    POST ${postUrl} HTTP/1.1
-    Content-Type: application/json
+			POST ${postUrl} HTTP/1.1
+			Content-Type: application/json
 
-            ${JSON.stringify({
-				ClienteId: item.idCliente,
-				VolumenYaComprado: item.volumenComprado,
-				Familia_x0020_de_x0020_ProductoId: item.idFamilia,
-				Proveedor_x0020_PrincipalId: item.idProveedorPrincipal,
-				Precio: item.precioPorMedida,
-				Condici_x00f3_nPago: item.condicionPago,
-				Periodo_x0020_de_x0020_CultivoId: item.idPeriodoCultivo,
-			})}
-    
-    --changeset_${changeID}--
-    `,
-	)}
+					${JSON.stringify({
+						ClienteId: item.idCliente,
+						VolumenYaComprado: item.volumenComprado,
+						Familia_x0020_de_x0020_ProductoId: item.idFamilia,
+						Proveedor_x0020_PrincipalId: item.idProveedorPrincipal,
+						Precio: item.precioPorMedida,
+						Condici_x00f3_nPago: item.condicionPago,
+						Periodo_x0020_de_x0020_CultivoId: item.idPeriodoCultivo,
+					})}
+			
+			--changeset_${changeID}--`
+	).join("")}
     
     --batch_${batchID}--
     `;
+	console.log(batchBody)
 
 	return { batchBody: batchBody, batchID: batchID };
 }
