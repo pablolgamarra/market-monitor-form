700eee327959f1a2556b3d83082696cddbe431f2
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-09-03 17:20:21 -0400

    Add monitorFormProducts strings

--- src/webparts/monitoreoMercadoForm/loc/default.js
+++ src/webparts/monitoreoMercadoForm/loc/default.js
@@ -1,14 +1,22 @@
-define([], function(){
-    return{
-        "PropertyPaneDescription": "Panel de Propiedades",
-        "BasicGroupName": "Nombre de Grupo",
-        "DescriptionFieldLabel": "Campo de Descripcion",
-        "PlaceholderUnidad": "Seleccione Unidad",
-        "PlaceholderCliente": "Seleccione Cliente",
-        "PlaceholderPeriodoCultivo": "Seleccione Periodo de Cultivo",
-        "Unidad": "Unidad",
-        "Cliente": "Cliente",
-        "PeriodoCultivo": "Periodo de Cultivo",
-        "PeriodoCultivoTitle":"Seleccione Periodo de Cultivo",
-    }
+define([], function () {
+	return {
+		PropertyPaneDescription: 'Panel de Propiedades',
+		BasicGroupName: 'Nombre de Grupo',
+		DescriptionFieldLabel: 'Campo de Descripcion',
+		PlaceholderUnidad: 'Seleccione Unidad',
+		PlaceholderCliente: 'Seleccione Cliente',
+		PlaceholderPeriodoCultivo: 'Seleccione Periodo de Cultivo',
+		Unidad: 'Unidad',
+		Cliente: 'Cliente',
+		PeriodoCultivo: 'Periodo de Cultivo',
+		PeriodoCultivoTitle: 'Seleccione Periodo de Cultivo',
+		GuardandoTitle: 'Guardando Datos',
+		GuardandoText: 'Por favor, espere.',
+		GuardarTitle: 'Guardado',
+		GuardarText: '¡Datos guardados correctamente!',
+		ErrorTitle: 'Error Guardando Datos',
+		ErrorText:
+			'Se ha producido un error al intentar guardar los datos. Por favor intente de nuevo',
+		VolumenYaCompradoLabel: 'Volumen Ya Comprado',
+	};
 });
--- src/webparts/monitoreoMercadoForm/loc/mystrings.d.ts
+++ src/webparts/monitoreoMercadoForm/loc/mystrings.d.ts
@@ -1,34 +1,49 @@
 declare interface IMonitoreoMercadoFormWebPartStrings {
-  PropertyPaneDescription: string;
-  BasicGroupName: string;
-  DescriptionFieldLabel: string;
-  AppLocalEnvironmentSharePoint: string;
-  AppLocalEnvironmentTeams: string;
-  AppLocalEnvironmentOffice: string;
-  AppLocalEnvironmentOutlook: string;
-  AppSharePointEnvironment: string;
-  AppTeamsTabEnvironment: string;
-  AppOfficeEnvironment: string;
-  AppOutlookEnvironment: string;
-  UnknownEnvironment: string;
+	PropertyPaneDescription: string;
+	BasicGroupName: string;
+	DescriptionFieldLabel: string;
+	AppLocalEnvironmentSharePoint: string;
+	AppLocalEnvironmentTeams: string;
+	AppLocalEnvironmentOffice: string;
+	AppLocalEnvironmentOutlook: string;
+	AppSharePointEnvironment: string;
+	AppTeamsTabEnvironment: string;
+	AppOfficeEnvironment: string;
+	AppOutlookEnvironment: string;
+	UnknownEnvironment: string;
 }
 
 declare module 'MonitoreoMercadoFormWebPartStrings' {
-  const strings: IMonitoreoMercadoFormWebPartStrings;
-  export = strings;
+	const strings: IMonitoreoMercadoFormWebPartStrings;
+	export = strings;
 }
 
-declare interface IMonitorFormHeaderStrings{
-  PlaceholderUnidad:string,
-  PlaceholderCliente:string,
-  PlaceholderPeriodoCultivo:string,
-  Unidad:string,
-  Cliente:string,
-  PeriodoCultivo:string,
-  PeriodoCultivoTitle:string,
+declare interface IMonitorFormHeaderStrings {
+	PlaceholderUnidad: string;
+	PlaceholderCliente: string;
+	PlaceholderPeriodoCultivo: string;
+	Unidad: string;
+	Cliente: string;
+	PeriodoCultivo: string;
+	PeriodoCultivoTitle: string;
 }
 
 declare module 'MonitorFormHeaderStrings' {
-  const value: IMonitorFormHeaderStrings;
-  export = value;
+	const value: IMonitorFormHeaderStrings;
+	export = value;
+}
+
+declare interface IMonitorFormProductsStrings {
+	GuardarTitle: string;
+	GuardarText: string;
+	ErrorTitle: string;
+	ErrorText: string;
+	GuardandoTitle: string;
+	GuardandoText: string;
+	VolumenYaCompradoLabel: string;
+}
+
+declare module 'MonitorFormProductsStrings' {
+	const value: IMonitorFormProductsStrings;
+	export = value;
 }
