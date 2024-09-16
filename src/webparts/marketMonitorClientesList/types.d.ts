885fc586924d65247d16ae1c58f1f65db714835f
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-09-16 14:31:27 -0400

    Add MarketMonitorClientesLists webPart

---
+++ src/webparts/marketMonitorClientesList/types.d.ts
@@ -0,0 +1,8 @@
+export interface Cliente {
+	Id: number | undefined;
+	Nombre: string | undefined;
+	CodigoSAP: number | undefined;
+	Unidad: Unidad | undefined;
+	CNG: CNG | undefined;
+	Anho: string;
+}
