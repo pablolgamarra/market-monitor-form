e3c4fef43b6bba1490c8f2ccae8c6d7989e464a6
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-10-02 14:31:49 -0400

    Added portuguese

---
+++ src/webparts/marketMonitorForm/loc/pt-br.js
@@ -0,0 +1,31 @@
+define([], function () {
+	return {
+		PropertyPaneDescription: 'Painel de propriedades',
+		BasicGroupName: 'Nome do grupo',
+		DescriptionFieldLabel: 'Campo de descrição',
+		PlaceholderUnidad: 'Selecionar Unidade',
+		PlaceholderCliente: 'Seleccione Cliente',
+		PlaceholderPeriodoCultivo: 'Selecionar período de corte',
+		Unidad: 'Unidade',
+		Cliente: 'Cliente',
+		PeriodoCultivo: 'Período de Cultivo',
+		PeriodoCultivoTitle: 'Seleccione Periodo de Cultivo',
+		GuardandoTitle: 'Salvando Dados',
+		GuardandoText: 'Por favor, espere.',
+		GuardarTitle: 'Salvo',
+		GuardarText: 'Dados salvos com sucesso!',
+		ErrorTitle: 'Erro ao salvar dados',
+		ErrorText:
+			'Ocorreu um erro ao tentar salvar dados. Por favor, tente novamente',
+		VolumenYaCompradoLabel: 'Volume já adquirido',
+		ApplicationErrorTitle: 'Erro no Aplicativo',
+		ApplicationErrorText: 'Emtre em contato com o departamento de T.I.',
+		UploadButtons: {
+			CNG: 'Carregar dados de CNG para DB',
+			Clientes: 'Carregar dados do clientes para o banco de dados',
+			Proveedores: 'Carregar dados do fornecedor para o banco de dados',
+			FamiliasProducto:
+				'Carregar dados da família de produtos para o banco de dados',
+		},
+	};
+});
